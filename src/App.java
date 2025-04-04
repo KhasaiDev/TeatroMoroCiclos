6a1d1c0b9b2ce1d9f1d9a5ad74b1b121ae89c9f5
KhasaiDev <khasaimassacre@gmail.com>
2025-04-04 13:14:22 -0300

    primer commit

---
+++ src/App.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        /*
+         * El ejercicio nos pide que mostremos una representacion visual de los asientos del teatro, para ello lo voy a abordar con una matriz.
+         * La voy a colocar por fuera para que al momento de hacer el loop del menu no se pierdan los datos de los asientos comprados.
+        */
+        char[][] asientos = {
+        {'*', '*', '*'},
+        {'*', '*', '*'},
+        {'*', '*', '*'} 
+        };
+
+        /*
+         * Vamos a crear una variable global para poder manejar el menu y que se repita hasta que el usuario quiera salir como pide el enunciado.
+        */
+        int opcion_usuario;
+
+        do {
+            System.out.println("\n--- TEATRO MORO ---");
+            System.out.println("Bienvenido al menu del teatro!");
+            /*
+             * Como nos pedian hacer un menu pero con el ciclo for se me ocurrio colocar las opciones en un array, de forma que si queremos agregar algo como
+             * "Ver Asientos" o "Ver obras" solo tenemos que colocarlo en el array y se va a mostrar por medio del for.
+            */
+            String[] opciones = {"Comprar entrada", "Salir"};
+            for (int i = 0; i < opciones.length; i++) {
+                System.out.println((i + 1) + ". " + opciones[i]);
+            }
+        
+            // Vamos a pedir la opcion del usuario
+            System.out.print("Seleccione una opción: ");
+            opcion_usuario = scanner.nextInt();
+        
+            // Para evaluar la opcion voy a usar un switch, ya que es mas entendible que un IF por cada opcion.
+            switch (opcion_usuario) {
+                case 1:
+                    comprarEntradas(asientos, scanner);
+                    break;
+                case 2:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+                    presionarEnterParaContinuar(scanner);
+                    break;
+            }
+        
+        } while (opcion_usuario != 2);
+    }
+
+    public static void comprarEntradas(char[][] asientos, Scanner scanner) {
+        //Ahora vamos a mostrar los asientos disponibles y que el suario eliga su opcion.
+        System.out.println("Estos son los asientos disponibles:");
+        for (int i = 0; i < asientos.length; i++) {
+            System.out.print("Fila " + (i + 1) + ": ");
+            for (int j = 0; j < asientos[i].length; j++) {
+                System.out.print(asientos[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //Ahora le solicitamos su eleccion
+        System.out.print("Ingrese la fila (1-3): ");
+        int fila = scanner.nextInt() - 1;
+
+        System.out.print("Ingrese el numero del asiento (1-3): ");
+        int asiento = scanner.nextInt() - 1;
+
+        //Ahora vamos a modificarlo visualmente para que al volver a entrar se vea ocupado.
+        if (asientos[fila][asiento] == '*') {
+            asientos[fila][asiento] = 'X';
+            System.out.println("Asiento reservado con éxito.");
+            System.out.println("Seras redirigido al menu principal.");
+            presionarEnterParaContinuar(scanner);
+        } else {
+            System.out.println("Ese asiento ya está ocupado.");
+            presionarEnterParaContinuar(scanner);
+        }
+
+    }
+
+    //Quise implementar este metodo para hacer mas comoda la interfaz de la terminal para que los mensajes se alcancen a leer y luego el usuario pueda continuar.
+    public static void presionarEnterParaContinuar(Scanner scanner) {
+        System.out.println("Presione Enter para continuar...");
+        scanner.nextLine(); // limpiar buffer
+        scanner.nextLine();
+    }
+}
